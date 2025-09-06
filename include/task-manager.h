3b4e65ed240ad38b683a1003fdc1be1a305e9f3d
Jacob Harwood <Jsbro98@github.com>
2025-09-05 19:40:59 -0500

    added and created task-manager header

---
+++ include/task-manager.h
@@ -0,0 +1,17 @@
+#ifndef TASK_MANAGER_H
+#define TASK_MANAGER_H
+
+#include "task.h"
+#include <vector>
+
+class TaskManager {
+  std::vector<Task> tasks;
+
+  public:
+    Task* get_task(int id);
+    void list_tasks();
+    bool mark_task(Task& task);
+    void create_task(int id, const std::string& desc);
+};
+
+#endif  // TASK_MANAGER_H
