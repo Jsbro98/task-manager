91dac46e2206a5242cf3b53561ac88489c376781
Jsbro98 <96179296+Jsbro98@users.noreply.github.com>
2025-09-14 01:30:20 -0500

    changed function declarations to have a file name paramater

--- include/file-handle.h
+++ include/file-handle.h
@@ -6,8 +6,8 @@
 #include <fstream>
 #include <string>
 
-void create_file();
-void write_tasks(const std::vector<Task>& tasks);
+void create_file(const std::string& file_name);
+void write_tasks(const std::vector<Task>& tasks, const std::string& file_name);
 std::vector<Task> read_tasks(const std::string& file_name);
 
 #endif  // FILE_HANDLE_H
