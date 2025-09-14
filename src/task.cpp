2076e870ee2cd3d60b5cff7c34d5f22c89e458cf
Jacob Harwood <Jsbro98@github.com>
2025-09-14 01:32:01 -0500

    implemented additional constructor

--- src/task.cpp
+++ src/task.cpp
@@ -3,6 +3,9 @@
 Task::Task(int id, const std::string& desc) 
 	: id(id), description(desc), completed(false) {}
 
+Task::Task(int id, const std::string& desc, bool completed)
+    : id(id), description(desc), completed(completed) {}
+
 int Task::get_id() const { return id; }
 
 const std::string& Task::get_description() const { return description; }
