dd8e605aca4648640d624087dac0b2189ce7f458
Jsbro98 <96179296+Jsbro98@users.noreply.github.com>
2025-09-16 20:52:50 -0500

    Moved manual tests to their own header/cpp file

---
+++ include/manual-tests.h
@@ -0,0 +1,6 @@
+#ifndef MANUAL_TESTS_H
+#define MANUAL_TESTS_H
+
+void execute_tests();
+
+#endif  // MANUAL_TESTS_H
---
+++ tests/manual-tests.cpp
@@ -0,0 +1,66 @@
+#include "task.h"
+#include "task-manager.h"
+#include "file-handle.h"
+#include "manual-tests.h"
+#include <iostream>
+
+void execute_tests() {
+  // test 1 scope
+  {
+    Task task{265, "Hello, World!"};
+
+    std::cout << task.get_description() << '\n';
+    std::cout << task.get_id() << '\n';
+    std::cout << std::boolalpha << task.is_completed() << '\n';
+
+    task.mark_completed();
+
+    std::cout << std::boolalpha << task.is_completed() << std::endl;
+  }
+
+  // test 2 scope
+  {
+    TaskManager man{};
+    man.create_task(451, "What's Up!");
+    man.create_task(225, "Hey There!");
+    man.create_task(265, "Yep, I'm Here!");
+
+    Task* task{man.get_task(225)};
+
+    if (task != nullptr) {
+      std::cout << task->get_description() << std::endl;
+    }
+
+    man.list_tasks();
+
+    man.mark_task(451);
+    task = man.get_task(451);
+
+    std::cout << std::boolalpha << "Task 451 is marked " << task->is_completed()
+              << std::endl;
+  }
+
+  // test scope 3
+  {
+    create_file("tasks.txt");
+
+    Task task1{6, "Check jira tickets"};
+    Task task2{7, "Look through logs", true};
+    Task task3{8, "Delete old cache"};
+
+    std::vector<Task> tasks{task1, task2, task3};
+
+    write_tasks(tasks, "tasks.txt");
+
+    tasks.clear();
+    tasks = read_tasks("tasks.txt");
+
+    for (const Task& task : tasks) {
+      std::cout << "ID: " << task.get_id() << '\n';
+      std::cout << "Desc: " << task.get_description() << '\n';
+      std::cout << std::boolalpha << "Completed: " << task.is_completed()
+                << '\n';
+      std::cout << '\n';
+    }
+  }
+}
