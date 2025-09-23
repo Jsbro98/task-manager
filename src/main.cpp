f3bc74b08a82cfaa4e71464564e45f665a44d6ef
Jsbro98 <96179296+Jsbro98@users.noreply.github.com>
2025-09-22 19:13:07 -0500

    created a new operator for Command usage in std::cout

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,37 @@
 #include "task-manager.h"
 #include "file-handle.h"
 #include "manual-tests.h"
+#include "io-handle.h"
 #include <iostream>
+#include <string>
+
+// make Command vars work with std::cout
+static std::ostream& operator<<(std::ostream& os, Command com) {
+	switch (com) { 
+	  case Command::Add:
+      os << "add";
+      break;
+    case Command::Mark:
+      os << "mark";
+      break;
+    case Command::Print:
+      os << "print";
+      break;
+    case Command::Remove:
+      os << "remove";
+      break;
+    default:
+      os << "invalid";
+      break;
+	}
+
+  return os;
+}
 
 int main() { 
-	execute_tests();
+	Command test{select_command()};
+
+	std::cout << "You inputted: " << test << std::endl;
 
 	return 0;
 }
