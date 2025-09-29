e67df373e61835f8b4918ee4c930d10ef0b7a9db
Jsbro98 <96179296+Jsbro98@users.noreply.github.com>
2025-09-28 21:47:17 -0500

    removing io functions. Controller will own this process.

--- include/io-handle.h
+++ include/io-handle.h
@@ -6,10 +6,6 @@
 enum class Command { Add, Mark, Remove, Print, Invalid };
 
 void print_commands();
-void add_command();
-void mark_command();
-void remove_command();
-void list_tasks_command();
 Command select_command();
 
 #endif  // IO_HANDLE_H
