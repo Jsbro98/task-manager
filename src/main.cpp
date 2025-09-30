4d6a4e606e005f48aef57fcab7a5fe14aca04958
Jsbro98 <96179296+Jsbro98@users.noreply.github.com>
2025-09-29 22:58:52 -0500

    beginnings of the first iteration

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "file-handle.h"
 #include "manual-tests.h"
 #include "io-handle.h"
+#include "controller.h"
 #include <iostream>
 #include <string>
 #include <filesystem>
@@ -36,15 +37,19 @@ int main() {
   const bool file_exist = std::filesystem::exists(path);
 
   if (!file_exist) create_file(file_name);
-  std::vector<Task> tasks{read_tasks(file_name)};
 
-  TaskManager tm{read_tasks(file_name)};
+  TaskManager tm{std::move(read_tasks(file_name))};
+  Controller::set_task_manager(tm);
 
-  std::cout << "Starting Task-Manager\n\n";
-  print_commands();
+  std::cout << "Starting Task-Manager\n";
+  std::cout << "type 'exit' to exit the program\n\n";
 
   while (true) {
     Command command{select_command()};
+
+    if (command == Command::Invalid) break;
+
+    Controller::dispatch(command);
   }
 
 	return 0;
