685be089809dee593d5afee38f258c8282dd0ea0
Jsbro98 <96179296+Jsbro98@users.noreply.github.com>
2025-10-09 20:00:42 -0500

    added a function to init the static task manager within Controller

--- include/controller.h
+++ include/controller.h
@@ -3,13 +3,16 @@
 
 #include "task-manager.h"
 #include "io-handle.h"
+#include <string>
+
+TaskManager make_task_manager(const std::string& file_name);
 
 class Controller {
   static TaskManager manager;
 
   public:
     static void dispatch(Command cmd);
-    static void set_task_manager(TaskManager& man);
+    static const std::vector<Task>& get_task_list();
 };
 
 #endif  // CONTROLLER_H
